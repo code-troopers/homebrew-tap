b373715a68e3cfe80d880b776047827525b0769b
Cedric Gatay <c.gatay@code-troopers.com>
2018-07-09 10:02:27 +0200

    ct - 1.0.4-pre

--- Formula/ct.rb
+++ Formula/ct.rb
@@ -1,9 +1,9 @@
 class Ct < Formula
 	desc "Command line wrapper for Code-Troopers"
 	homepage "https://github.com/code-troopers/ct/"
-	url "https://github.com/code-troopers/ct/releases/download/v0.1.3-pre/ct-v0.1.3-pre-x86_64-apple-darwin.tar.gz"
-	version "0.1.3-pre"
-	sha256 "950b825891f8773b6d73e90971a127ec81d809955bcd29b055a44b4de1927ff8"
+	url "https://github.com/code-troopers/ct/releases/download/v0.1.4-pre/ct-v0.1.4-pre-x86_64-apple-darwin.tar.gz"
+	version "0.1.4-pre"
+	sha256 "e74eadb85457580986ed34d088084a5613bac80977a924187b5f8c34a84d4770"
 
 	def install
 		bin.install "ct"
